6b22d80ecdaac269df7184d158c19cac151a86d3
Moueez Aleem <encyphersdev1@gmail.com>
2025-10-22 19:09:20 +0500

    test7

--- app.py
+++ app.py
@@ -165,6 +165,9 @@ def get_token():
 
         response = jsonify(response_data)
         # response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
         return response
 
     except Exception as e:
